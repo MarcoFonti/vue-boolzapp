1d1869ba7849eb54f119a34c184ff7acdb56afd4
MarcoFonti <theemporiumg@gmail.com>
2024-01-24 19:22:43 +0100

    add milestone 1 finished

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 // CHECK
 console.log('JS VUE OK', Vue);
+
+// ESTRAPOLO FUNZIONE DA VUE
+const {createApp} = Vue;
+
+// CREO VARIBILE E RICHIAMO LA FUNZIONE
+const app = createApp ({
+    name: 'MiaApp',
+    // USO DATA CON ALL'INTENRO IL RETURN 
+    data() {
+        return {
+            date: data,
+        }
+    }
+})
+
+// COLLEGO ID 
+app.mount('#root');
