64fd2b3e94514230c072c52daa1674a13a3d8e8a
MarcoFonti <theemporiumg@gmail.com>
2024-01-26 21:49:28 +0100

    add changes to inaccutacies at milestone 2-3

--- js/script.js
+++ js/script.js
@@ -6,82 +6,94 @@ const {createApp} = Vue;
 
 // CREO VARIBILE E RICHIAMO LA FUNZIONE
 const app = createApp ({
-    name: 'MiaApp',
+    name: 'WhatsApp',
     // USO DATA CON ALL'INTENRO IL RETURN 
     data() {
         return {
+            // DATI UTENTE 
             user: data.user,
+            // DATI CONTATTI
             contacts: data.contacts,
-            active: '',
-            write: '',
-
+            // UTENTE SELEZIONATO
+            active: 1,
+            // TESTO INPUT MESSAGGIO
+            write:''
         }
     },
 
-    computed:{
-       
-        selectedUser() {
-          
-        }
-
-    },
-
-    methods: {
-
-        // CONFRONTO ID 
-        currentUser(id){
-        const userClick = this.contacts.filter((user) => {
-            if(id !== user.id) {
-                return false
-                } else {
+    // COME VORREI CHE ...
+    computed: {
+        // CONTATTO SELEZIONATO
+        selectedContact(){
+            // INDIVIDUO ELEMENTO SPECIFICO
+            const contactCycle = this.contacts.find((element) => {
+                // SE ELEMENTO ID E' IDENTICO
+                if(element.id === this.active) {
                     return true
-                }
-            })
+                } else {
+                    return false
+                }   
+            }) 
 
-            console.log(userClick)
+        return contactCycle; 
 
-            this.active = userClick
+        }
+    },
 
+    // FUNZIONI
+    methods:{
+
+        // COLORE MESSAGGI
+        messageColor(status) {
+            // SE STATUS E'IDENTICO
+            if(status === 'sent') {
+               return 'message-sent'
+            } else {
+               return 'message-received'  
+            }
         },
 
-        // CREI MESSAGGIO 
+        // CREA MESSAGGIO
         createMessages() {
+            // SE NON C'E' TESTO
+            if(!this.write) return;
+
+            // CREO OGETTO
             const newMessages = 
             {
-                id: 10,
-                date: '10/01/2020 15:30:55',
+                id: new Date().getTime(),
+                date: new Date().toLocaleString(),
                 text: this.write,
                 status: 'sent'
             }
 
-            this.contacts.forEach(element => {
-                element.messages.push(newMessages)
-            });
+            // METTO OGGETTO NELLA COMPUTED CONTATTO SELEZIONATO
+            this.selectedContact.messages.push(newMessages);
+
+            this.write = '';
             
         },
 
         // RICEVI MESSAGGIO
         receivedMessages() {
 
+            // METODO DOPO TOT SECONDI
             setTimeout(() => {
 
+            // CREO OGETTO
             const answer = 
-
             {
-                id: 90,
-                date: '10/01/2020 15:30:55',
+                id: new Date().getTime(),
+                date: new Date().toLocaleString() ,
                 text: 'Ok',
                 status: 'received'
             }
+            // METTO OGGETTO NELLA COMPUTED CONTATTO SELEZIONATO
+            this.selectedContact.messages.push(answer);
 
-            this.contacts.forEach(element => {
-                element.messages.push(answer)
-            });
-
-            },3000)
+            },3000) // RITARDO
             
         },
-
     }
 
 })
